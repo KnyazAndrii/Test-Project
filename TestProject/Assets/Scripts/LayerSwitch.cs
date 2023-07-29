dae99783cd309761a2afe90fda943bc8e5867e5b
Knyaz_Andrii <andrijko.zagaruk@gmail.com>
2023-07-29 11:09:52 +0300

    Change Hole type
    Changed hole to square form and block can drop into the hole

---
+++ TestProject/Assets/Scripts/LayerSwitch.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSwitch : MonoBehaviour
+{
+    private string _enterLayer = "NoCollisions";
+    private string _exitLayer = "Default";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.layer = LayerMask.NameToLayer(_enterLayer);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        other.gameObject.layer = LayerMask.NameToLayer(_exitLayer);
+    }
+}
